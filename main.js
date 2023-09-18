cdb8fb4cbad98eee5b424e5b8234a4519adb979e
GooberFishey <gooberfishey2009@gmail.com>
2023-09-18 17:16:37 -0500

    first js

---
+++ main.js
@@ -0,0 +1,11 @@
+const STARTING_POKER_CHIPS= 100; // points
+const NUMBER_OF_PLAYERS = 3;
+const NUMBER_OF_STARTING_CARDS = 2;
+let playerOnePoints = STARTING_POKER_CHIPS;
+let playerTwoPoints = STARTING_POKER_CHIPS;
+let playerThreePoints = STARTING_POKER_CHIPS;
+
+playerOnePoints -= 50;
+playerTwoPoints -=25;
+playerThreePoints +=75;
+

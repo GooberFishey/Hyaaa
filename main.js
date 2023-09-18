406f89c531be8d1785376f479f7998667c1d8fce
GooberFishey <gooberfishey2009@gmail.com>
2023-09-18 17:36:48 -0500

    s

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ const NUMBER_OF_STARTING_CARDS = 2;
 let playerOnePoints = STARTING_POKER_CHIPS;
 let playerTwoPoints = STARTING_POKER_CHIPS;
 let playerThreePoints = STARTING_POKER_CHIPS;
-console.log(`Welcome to Texas Hold'em. The championship title will be awarded to one of these three players: ${playerOneName}, ${playerTwoName} and ${playerThreeName}. Each player has ${STARTING_POKER_CHIPS} in their pot. We have an exciting game ahead of us. May the best player win!`)
+
 let playerOneName = "Chloe";
 let playerTwoName = "Jasmine";
 let playerThreeName = "Jen";
+
+console.log(`Welcome to Texas Hold'em. The championship title will be awarded to one of these three players: ${playerOneName}, ${playerTwoName} and ${playerThreeName}. Each player has ${STARTING_POKER_CHIPS} in their pot. We have an exciting game ahead of us. May the best player win!`)
 
 playerOnePoints -= 50;
 playerTwoPoints -=25;

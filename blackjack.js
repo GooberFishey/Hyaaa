2ff3b407f880150bd443bc8729475965c4e554ff
GooberFishey <gooberfishey2009@gmail.com>
2023-09-22 14:09:19 -0500

    gf

--- blackjack.js
+++ blackjack.js
@@ -1,7 +1,11 @@
-let cardOne=7;
-let cardTwo=5;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+let cardOne= getRandomInt(14);
+let cardTwo= getRandomInt(14);
 let sum = cardOne+cardTwo;
-let cardThree = 7;
+let cardThree = getRandomInt(14);
 
 sum += cardThree;
 if (sum > 21) {

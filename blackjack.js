26801eb6a3c72f1acb9e8af21844188dcd488285
GooberFishey <gooberfishey2009@gmail.com>
2023-09-23 17:07:10 -0500

    dasfc

--- blackjack.js
+++ blackjack.js
@@ -4,12 +4,19 @@ function getRandomInt(max) {
 
 let cardOne= getRandomInt(14);
 let cardTwo= getRandomInt(14);
-let sum = cardOne+cardTwo;
+let sum = cardOne+cardTwo+cardThree;
 let cardThree = getRandomInt(14);
+let houseOne = getRandomInt(14);
+let houseTwo = getRandomInt(14);
+let houseThree = getRandomInt(14);
+let houseSum = houseOne+houseTwo+houseThree;
 
-sum += cardThree;
-if (sum > 21) {
-  console.log('You lost');
+if (sum > 21 || sum < houseSum) {
+  console.log('You have ${sum} points. House has ${houseSum} points. You loose!');
   process.exit(1); // exit program
 }
-console.log(`You have ${sum} points`);
+
+if (sum > houseSum && sun < 21 || houseSum > 21) {
+    console.log(`You have ${sum} points. House has ${houseSum} points. You Win!`);
+    process.exit(1);
+}

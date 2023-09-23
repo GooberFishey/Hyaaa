1bdf7171ec52d0773a0718b2b41e55e8d7102b87
GooberFishey <gooberfishey2009@gmail.com>
2023-09-23 17:07:45 -0500

    ascx# Please enter the commit message for your changes. Lines starting

--- blackjack.js
+++ blackjack.js
@@ -4,12 +4,12 @@ function getRandomInt(max) {
 
 let cardOne= getRandomInt(14);
 let cardTwo= getRandomInt(14);
-let sum = cardOne+cardTwo+cardThree;
 let cardThree = getRandomInt(14);
 let houseOne = getRandomInt(14);
 let houseTwo = getRandomInt(14);
 let houseThree = getRandomInt(14);
 let houseSum = houseOne+houseTwo+houseThree;
+let sum = cardOne+cardTwo+cardThree;
 
 if (sum > 21 || sum < houseSum) {
   console.log('You have ${sum} points. House has ${houseSum} points. You loose!');
